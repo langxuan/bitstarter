cf6ad60be12565e466c6dd8695fab8bab1d8f2fe
Hal(langxuan) <lunhui.langxuan@gmail.com>
2013-07-18 00:04:16 +0800

    style the website

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync('index.html');
-
 app.get('/', function(request, response) {
+  var buf = fs.readFileSync(htmlfile);
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
